23e0277da24048dad0243c866597fada99c8e71f
이주희 <klkl0812@naver.com>
2023-07-02 14:21:57 +0900

    Java 공부기록

---
+++ java study/src/javajungsuk7_10/Ch07_51a52.java
@@ -0,0 +1,21 @@
+package javajungsuk7_10;
+
+public class Ch07_51a52 {
+    
+    public static void main(String[] args) {
+        // 7-51 ~ 7-52 익명 클래스
+
+        // 정의와 생성을 동시에 하는 이름이 없는 일회용 클래스를 말한다.
+
+        System.out.println("클래스 이름 대신 조상의 이름을 빌려쓴다. 인터페이스는 구현 인터페이스의 이름을 쓴다.");
+        System.out.println("한 번만 쓸 것이기때문에 정의와 생성을 동시에 한다.");
+    }
+}
+
+// new Object() {
+
+// }
+
+// new interface() {
+    
+// }
